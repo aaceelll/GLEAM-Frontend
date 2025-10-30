f20437bd7e45c3f72c8e64c6b9d145db1f894ecb
Rachel Savitri <savitri.rachel12@gmail.com>
2025-10-30 19:50:54 +0700

    revert

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ForgotPasswordForm } from "@/components/forms/forgot-password-form";
-export const dynamic = "force-dynamic";
 
 export default function ForgotPasswordPage() {
   return (
